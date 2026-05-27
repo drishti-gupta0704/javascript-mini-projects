fa6d05f950765918fa67f8ccab197036440ddf6a
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-05-27 17:40:45 +0530

    changing the content of html through javascript event handling

--- button_click_counter/script.js
+++ button_click_counter/script.js
@@ -1,1 +1,11 @@
 
+let countText = document.getElementById("count");
+let button = document.getElementById("btn");
+
+let count = 0;
+
+button.addEventListener("click", function () {
+    count++;
+    countText.innerText = "Count: " + count;
+
+});
