a46599ff1ff90e1b61fbec2b2d03932edf7d7517
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-05-27 17:50:30 +0530

    created new function and UI change on clicking buttons

--- button_click_counter/script.js
+++ button_click_counter/script.js
@@ -1,11 +1,62 @@
 
 let countText = document.getElementById("count");
-let button = document.getElementById("btn");
+
+let increaseBtn = document.getElementById("increaseBtn");
+let decreaseBtn = document.getElementById("decreaseBtn");
+let resetBtn = document.getElementById("resetBtn");
+let message = document.getElementById("message");
 
 let count = 0;
 
-button.addEventListener("click", function () {
+
+
+function updateUI() {
+
+    countText.innerText = count;
+
+    if (count > 0) {
+        countText.style.color = "green";
+    }
+
+    else if (count < 0) {
+        countText.style.color = "red";
+    }
+
+    else {
+        countText.style.color = "black";
+    }
+
+    if (count === 10) {
+        message.innerText = "Great! Count reached 10";
+    }
+
+    else if (count === -10) {
+        message.innerText = "Count reached -10";
+    }
+
+    else {
+        message.innerText = "";
+    }
+
+}
+
+
+increaseBtn.addEventListener("click", function () {
     count++;
-    countText.innerText = "Count: " + count;
+    updateUI();
+
+});
+
+
+decreaseBtn.addEventListener("click", function () {
+    count--;
+    updateUI();
+
+});
+
+
+resetBtn.addEventListener("click", function () {
+    count = 0;
+    updateUI();
 
 });
